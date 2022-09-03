59629788d2074a4981e6562a916aefaa7d97e48c
Daniel Sadiq <danielsadiq93@gmail.com>
2022-09-03 18:10:00 +0100

    SHOWING PROMISE ANEW

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ login_manager.init_app(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+uri = os.environ.get("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
